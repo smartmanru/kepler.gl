f1c44d8215b0a1b1d6114119c7eea0edb5f6f1da
Shan He <heshan0131@gmail.com>
2019-04-05 11:36:42 -0700

    [Feat] Gpu data filter 2 (#464)
    - refactor filterData to filterDataset
    - filter dataset based on gpu or cpu mode
    - set gpuChannel in filter and gpuFilter in dataset to be passed to layer

--- src/layers/icon-layer/icon-layer.js
+++ src/layers/icon-layer/icon-layer.js
@@ -267,7 +267,7 @@ export default class IconLayer extends Layer {
       ...(this.config.visConfig.fixedRadius ? {} : {radiusMaxPixels: 500})
     };
 
-    return [
+    return this.iconGeometry ? [
       new SvgIconLayer({
         ...layerProps,
         ...data,
@@ -314,6 +314,6 @@ export default class IconLayer extends Layer {
           })
         ]
       : [])
-    ];
+    ] : [];
   }
 }
